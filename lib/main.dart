4a1ccb67882302380ed033ce1f36ef83f66a1aea
Alex Bordei <alex@whiz.ro>
2022-02-23 09:54:25 +0200

    Initial commit - update

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'GDG Cluj',
-      home: Text('Welcome to GDG Cluj!'),
+      home: Center(
+        child: Text('Welcome to GDG Cluj!'),
+      ),
     );
   }
 }
