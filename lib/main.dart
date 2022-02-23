13f2fb8a656b518cb7926fc63f9993dd72457639
Alex Bordei <alex@whiz.ro>
2022-02-23 10:28:33 +0200

    Session 1 code added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,10 +11,47 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'GDG Cluj',
-      home: Center(
-        child: Text('Welcome to GDG Cluj!'),
+      home: Scaffold(
+        backgroundColor: Colors.grey,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Image.asset('assets/gdg_cluj.png'),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Text(
+              'Welcome to\nGDG Cluj!',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+                fontSize: 30,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Made with ❤ in',
+                  style: TextStyle(fontSize: 20),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                SvgPicture.asset(
+                  'assets/flutter.svg',
+                  width: 150,
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
