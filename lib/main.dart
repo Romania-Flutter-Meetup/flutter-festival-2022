0e3ede2242d386493d48912430cf1b28b49111ae
Alex Bordei <alex@whiz.ro>
2022-02-28 20:25:40 +0200

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -15,42 +15,55 @@ class MyApp extends StatelessWidget {
       title: 'GDG Cluj',
       home: Scaffold(
         backgroundColor: Colors.grey,
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Image.asset('assets/gdg_cluj.png'),
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            Text(
-              'Welcome to\nGDG Cluj!',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-                fontSize: 30,
+        body: SafeArea(
+          child: Column(
+            children: [
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Image.asset('assets/gdg_cluj.png'),
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Text(
+                      'Welcome to\nGDG Cluj!',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 30,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Made with ❤ in',
-                  style: TextStyle(fontSize: 20),
-                ),
-                SizedBox(
-                  width: 10,
-                ),
-                SvgPicture.asset(
-                  'assets/flutter.svg',
-                  width: 150,
-                )
-              ],
-            ),
-          ],
+              Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Made with ❤ in',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      SvgPicture.asset(
+                        'assets/flutter.svg',
+                        width: 150,
+                      )
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
