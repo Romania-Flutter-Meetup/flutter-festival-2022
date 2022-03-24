a57737d2a1757497d328e3864c573e2d9d615f83
Alex Bordei <alex@whiz.ro>
2022-03-24 16:27:43 +0200

    updates

--- lib/screens/event_screen.dart
+++ lib/screens/event_screen.dart
@@ -1,11 +1,6 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_festival_romania_meetup_2022/models/photo_model.dart';
 import 'package:flutter_html/flutter_html.dart';
-import 'package:http/http.dart';
 
-import '../helpers/network.dart';
 import '../models/event_model.dart';
 
 class EventScreen extends StatefulWidget {
@@ -16,40 +11,10 @@ class EventScreen extends StatefulWidget {
 }
 
 class _EventScreenState extends State<EventScreen> {
-  bool _isLoaded = false;
-  List<PhotoModel> photos = [];
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  Future<void> initApp() async {
-    await loadData();
-    setState(() {
-      _isLoaded = true;
-    });
-  }
-
-  Future<void> loadData() async {
-    Response response = await Network.loadPhotos(event_id: args.id ?? '');
-
-    if (response.statusCode == 200) {
-      List<dynamic> photos = jsonDecode(response.body);
-      print(response.body);
-      setState(() {
-        photos = photos.map((e) => PhotoModel.fromJson(e)).toList();
-      });
-    }
-    //TODO: treat the error
-  }
-
   @override
   Widget build(BuildContext context) {
     final args = ModalRoute.of(context)!.settings.arguments as EventModel;
-    if (!_isLoaded) {
-      initApp();
-    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text(args.name ?? ''),
